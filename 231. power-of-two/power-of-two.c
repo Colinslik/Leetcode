591b3cacce63ab3c33337d474ce789ac8cadab31
Colin Lin <Colinslik@users.noreply.github.com>
2019-02-01 12:27:06 +0800

    Create power-of-two.c

---
+++ 231. power-of-two/power-of-two.c
@@ -0,0 +1,18 @@
+bool isPowerOfTwo(int n) {
+    double base = log(n);
+    double divider = log(2);
+    int coefficient;
+    char temp[10];
+    base /= divider;
+    sprintf(temp,"%.9f",base);
+    base = atof(temp);
+    /*sprintf(temp,"%.7f",log(n));
+    base = atof(temp);
+    sprintf(temp,"%.7f",log(3));
+    divider = atof(temp);
+    base /= divider;*/
+    coefficient = (int)base;
+    //printf("base:%f divider:%f\n",base,divider);
+    //printf("%.20f   %d\n",base,coefficient);
+    return (base == coefficient)?1:0;
+}
